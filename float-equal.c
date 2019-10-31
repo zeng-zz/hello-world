9748ab30e4e7b6ff18f6408ed14cfe0c4ba6da23
zeng-zz <1220147383@qq.com>
2019-10-31 19:19:24 +0800

    float-equal

--- float-equal.c
+++ float-equal.c
@@ -1,12 +1,14 @@
 //实现代码判断2个float变量相等
 //误差精确到时小数点后5位
 #include <stdio.h>
+#include <math.h>
+#define EPSILON 0.00001
 int main()
 {
     float m = 1.78;
     float x = 1.779999, y = 2;
     float z = m / y, z1 = x / y;
-    if (z == z1) {
+    if (fabs(z-z1)<EPSILON) {
         z = (m + x) / y;
     }
     printf("Z is %f\n", z);
